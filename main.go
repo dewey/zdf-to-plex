9f94bad12fbf7b386a12a3efd8927433dc8451cb
Philipp Defner <philipp.defner@justwatch.com>
2022-01-05 10:28:56 +0100

    Progress bar as newline

--- main.go
+++ main.go
@@ -121,6 +121,8 @@ func main() {
 		args = append(args, "--concurrent-fragments")
 		args = append(args, "10")
 		args = append(args, "-o")
+		// Progress bar as new lines, so that it looks more readable in log files
+		args = append(args, "--newline")
 		args = append(args, t.OutputFile)
 		args = append(args, t.URL)
 		cmd := exec.Command(*youtubedlBinPath, args...)
